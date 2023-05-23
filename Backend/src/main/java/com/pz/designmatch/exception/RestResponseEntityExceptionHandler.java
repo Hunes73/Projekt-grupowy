21b0177fdf66f2e18abbf3003c9c0cd11646c085
Jakub Kasinski <jkasinski73@gmail.com>
2023-05-24 00:13:45 +0200

    Update RestResponseEntityExceptionHandler.java

--- Backend/src/main/java/com/pz/designmatch/exception/RestResponseEntityExceptionHandler.java
+++ Backend/src/main/java/com/pz/designmatch/exception/RestResponseEntityExceptionHandler.java
@@ -37,18 +37,3 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return handleExceptionInternal(ex, new ApiResponse(false, error), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
-//@ControllerAdvice
-//public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-//
-//    @ExceptionHandler(value = {UsernameNotFoundException.class, ArtistProfileNotFound.class})
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public ErrorObject handleUsernameNotFoundException(Exception ex) {
-//        return new ErrorObject(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
-//    }
-//
-//    @ExceptionHandler(value = RuntimeException.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public ErrorObject handleRuntimeException(RuntimeException ex) {
-//        return new ErrorObject(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), new Date());
-//    }
-//}
